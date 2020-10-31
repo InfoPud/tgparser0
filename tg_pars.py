f8dcc95e62827c5a43b8050428b4fa352ec77993
InfoPud <65897793+InfoPud@users.noreply.github.com>
2020-10-31 20:48:55 +0300

    Add files via upload

---
+++ tg_pars.py
@@ -0,0 +1,16 @@
+# coding-utf-8
+from telethon import TelegramClient, events
+import os
+from pars_conf import account, list_all
+api_id = os.environ.get('apiid0')
+api_hash = os.environ.get('api_hash0')
+client = TelegramClient('my_account', api_id, api_hash)
+@client.on(events.NewMessage)
+async def my_event_handler(event):
+        if event.chat.username in list_all:
+                chat = await event.get_input_chat()
+                msg = await client.get_messages(chat.channel_id, limit=1)
+                await client.forward_messages(int(account[2]), msg)
+                print("busted")
+client.start()
+client.run_until_disconnected()
